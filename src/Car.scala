3d9bf6816d20576ab5928a50705f68bd570393ef
Joe Morris <joe@mpb.local>
2016-10-19 08:34:17 -0500

    Second commit. Removed redundant code

--- src/Car.scala
+++ src/Car.scala
@@ -13,14 +13,6 @@ class Car {
     println(s"Your engine is $engine liters")
     println(s"You have $seats in your car")
   }
-
-  def carDetails(wheels: Int, engine: Int, gas: Int, seats: Int) {
-    println(s"You have $wheels wheels on your car")
-    println(s"You have $gas gallons in your car")
-    println(s"Your engine is $engine liters")
-    println(s"You have $seats in your car")
-  }
-
 }
 
 
